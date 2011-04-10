87189a69236de3ecc28cb68385a450cebfd0a89f
bhuvanesh selvaraj <iambhuvanesh@gmail.com>
2011-04-10 19:29:15 +0530

    added profile feature to signup form

--- PHP/signup_user.php
+++ PHP/signup_user.php
@@ -26,7 +26,7 @@ $result = mysql_query($query) or die('Query failed. ' . mysql_error());
 if($result)
 	{
 		echo "registration successful";
-		header('Location:profile.php');
+		header('Location:profile.php?user_email='.$u_email);
 	}
 else
 {echo "registration failed";}
