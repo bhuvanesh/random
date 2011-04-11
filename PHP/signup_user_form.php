ce4e3130fe2a2c0aefc66a7765e14cbfecf44e77
bhuvanesh selvaraj <iambhuvanesh@gmail.com>
2011-04-11 23:43:23 +0530

    sign_up form styled using css

--- PHP/signup_user_form.php
+++ PHP/signup_user_form.php
@@ -1,15 +1,22 @@
 <html>
 <body>
+<link href="../CSS/signup_form.css" rel="stylesheet" type="text/css" />
 <form action="signup_user.php" method="POST"><br/>
-Name     : <input type="text" name="name"><br/>
-Password : <input type="password" name="pass"><br/>
-email    : <input type="text" name="email"><br/>
-age 	 : <input type="text" name="age"><br/>
+<fieldset>
+<legend>Sign-up Form</legend>
+<p><label for="name">Name : </label><input type="text" name="name"><br/></p>
+<p><label for="password">Password : </label><input type="password" name="pass"><br/></p>
+<p><label for="email">Email : </label><input type="text" name="email"><br/></p>
+<p><label for="name">Age : </label><input type="text" name="age"><br/></p>
  <?php
           require_once('../captcha/recaptchalib.php');
           $publickey = "6LcaTsMSAAAAAPSr6W_yqVszxL-EqmKNaPvrgewp"; // you got this from the signup page
           echo recaptcha_get_html($publickey);
         ?>
 
-<input type="submit" value="submit" name="submit"><br />
-</form><br /> 
+<br/>
+<input type="submit" value="submit" name="submit"><br/>
+</fieldset>
+</form>
+</body>
+</html>
